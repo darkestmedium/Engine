5c7b7bde7535005feec4fa936fc7b667af0d084f
darkestmedium <darkestmedium@gmail.com>
2024-10-15 22:45:36 +0200

    Adds starter code and partial third party libraries.

---
+++ Source/Runtime/vk_types.h
@@ -0,0 +1,12 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#define FMT_HEADER_ONLY
+#include <fmt/core.h>
+
+
+//we will add our main reusable types here
